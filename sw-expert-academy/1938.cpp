eda84b5ffded80d5248152a622bb53fd1ce184fd
h3136514 <3136514@naver.com>
2023-11-01 10:16:49 +0900

    feat: 아주 간단한 계산기

---
+++ sw-expert-academy/1938.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+int a, b;
+int main(){
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+
+    return 0;
+}
