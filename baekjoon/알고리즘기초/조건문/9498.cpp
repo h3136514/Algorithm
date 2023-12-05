8533d3416c1b770a5ae0185f2372103e86ac355c
h3136514 <3136514@naver.com>
2023-12-05 21:05:50 +0900

    feat: 시험 성적

---
+++ baekjoon/알고리즘기초/조건문/9498.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+int a;
+
+int main(){
+    scanf("%d", &a);
+    if(a>=90)
+        printf("A");
+    else if(a>=80)
+        printf("B");
+    else if(a>=70)
+        printf("C");
+    else if(a>=60)
+        printf("D");
+    else
+        printf("F");
+        
+    return 0;
+}
