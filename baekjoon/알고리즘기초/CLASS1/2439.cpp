3873118db6c791573406864ae52a87909003c916
h3136514 <3136514@naver.com>
2023-12-13 22:17:50 +0900

    feat: 별 찍기 - 2

---
+++ baekjoon/알고리즘기초/CLASS1/2439.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+int n, i, j;
+
+int main(){
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+        for(j=n;j>i;j--)
+            printf(" ");
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+        
+    return 0;
+}
