aae7514082aaffdfb79863c237e915348a5d1cf6
h3136514 <3136514@naver.com>
2023-12-17 11:41:12 +0900

    feat: 새싹

---
+++ baekjoon/알고리즘기초/CLASS1/25083.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+
+int main(){
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+    return 0;
+}
