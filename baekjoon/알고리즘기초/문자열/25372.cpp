80671afeb267a00183e22cf96303ed041f37e1b9
h3136514 <3136514@naver.com>
2024-03-28 22:16:22 +0900

    feat: Four Squares

--- baekjoon/알고리즘기초/문자열/25372.cpp
+++ baekjoon/알고리즘기초/문자열/25372.cpp
@@ -1,16 +1,16 @@
 #include<cstdio>
-#include<cstring>
-int i, N;
-char str[1001]; 
+#include<algorithm>
+using namespace std;
+int N, i, j, dp[50001];
 
-int main(){
+int main() {
     scanf("%d", &N);
-    for(i=0;i<N;i++){
-        scanf("%s", &str);
-        if(strlen(str)>= 6 && strlen(str)<= 9){
-            printf("yes\n");
-        } else
-            printf("no\n");
+    for(i=1;i<=N;i++){
+        dp[i] = dp[i-1] +1;
+        for(j=1; j*j <=i;j++){
+            dp[i] = min(dp[i], dp[i-j*j]+1);
+        }
     }
+    printf("%d\n", dp[N]);
     return 0;
 }
