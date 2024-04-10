062ab0e0111503b23937b5879d5169688b5bb343
h3136514 <3136514@naver.com>
2024-04-10 23:20:12 +0900

    feat: 별 찍기 - 5

---
+++ baekjoon/구현/2442.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+int N, i, j;
+
+int main() {
+    scanf("%d", &N);
+    for(i=1;i<=N;i++){
+        for(j=i;j<N;j++)
+            printf(" ");
+        for(j=1;j<=2*i-1;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
