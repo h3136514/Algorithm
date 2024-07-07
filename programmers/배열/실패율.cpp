41ff1ddef6d0c33ed61f6a86024199b439d90676
h3136514 <3136514@naver.com>
2024-07-07 23:52:09 +0900

    style: 주석 추가

--- programmers/배열/실패율.cpp
+++ programmers/배열/실패율.cpp
@@ -9,6 +9,7 @@ int challenge[501];
 
 // 정렬 기준 정의
 bool compare(pair<int, float> a, pair<int, float> b) {
+    // 실패율이 같으면
     if(a.second == b.second){
         return a.first < b.first;
     }
@@ -16,10 +17,11 @@ bool compare(pair<int, float> a, pair<int, float> b) {
     return a.second > b.second;
 }
 
+// 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 반환(N:스테이지 개수, M:스테이지 길이, 시간복잡도 : O(N*M + NlogN))
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     
-    // 각 스테이지의 실패한 인원수와, 도전한 적이 있는 인원수를 구함 
+    // 각 스테이지의 실패한 인원수와, 도전한 적이 있는 인원수를 구함, O(N*M)
     for(int i = 0; i < stages.size(); i++ ) {
         fail[stages[i]]++;
         for(int j = 1; j <= stages[i]; j++ ) {
@@ -39,7 +41,8 @@ vector<int> solution(int N, vector<int> stages) {
             failRate[i].second = (float)fail[i + 1]/challenge[i + 1];
        
     }
-    
+
+    // 정렬:O(NlogN)
     sort(failRate.begin(), failRate.end(), compare);
     for(int i = 0; i < failRate.size(); i++) {
         answer.push_back(failRate[i].first);
