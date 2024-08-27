2d887b3f88857a08607322d37557902fbfda4de9
h3136514 <3136514@naver.com>
2024-08-27 17:09:31 +0900

    feat: N과 M (6)

---
+++ baekjoon/알고리즘기초/백트래킹/15655.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int N, M, num[9], arr[9];
+
+void dfs(int cnt, int maxNum) {
+    if(cnt == M) {
+        for(int i = 0; i < M; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+
+        return;
+    }
+
+    for(int i = 0; i < N; i++) {
+        if(num[i] > maxNum) {
+            maxNum = num[i];
+            arr[cnt] = num[i];
+            dfs(cnt+1, maxNum);
+        }
+    }
+}
+
+//자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하여라(단, N의 수열은 다 다른수이고 고른 수열은 오른차순이어야 한다.)
+int main() {
+    cin >> N >> M;
+    for(int i = 0; i < N; i++)
+        cin >> num[i];
+
+    sort(num, num+N);
+    dfs(0, 0);
+
+    return 0;
+}
