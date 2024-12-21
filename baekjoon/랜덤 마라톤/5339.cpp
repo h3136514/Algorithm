d3aa1d814eb769cd7bd54d7ba313268540ba96aa
h3136514 <3136514@naver.com>
2024-12-21 10:44:21 +0900

    feat: 콜센터

---
+++ baekjoon/랜덤 마라톤/5339.cpp
@@ -0,0 +1,20 @@
+// 구현
+#include <iostream>
+
+using namespace std;
+
+// 콜센터
+int main(){
+    cout << "     /~\\\n";
+    cout <<"    ( oo|\n";
+    cout <<"    _\\=/_\n";
+    cout <<"   /  _  \\\n";
+    cout <<"  //|/.\\|\\\\\n";
+    cout <<" ||  \\ /  ||\n";
+    cout <<"============\n";
+    cout <<"|          |\n";
+    cout <<"|          |\n";
+    cout <<"|          |\n";
+
+    return 0;
+}
