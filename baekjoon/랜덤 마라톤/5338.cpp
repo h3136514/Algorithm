cecd16743e9a27d800dcd23e4d415a5d32d45bca
h3136514 <3136514@naver.com>
2025-01-26 23:56:17 +0900

    feat: 마이크로소프트 로고

---
+++ baekjoon/랜덤 마라톤/5338.cpp
@@ -0,0 +1,15 @@
+// 구현
+#include <iostream>
+
+using namespace std;
+
+// 마이크로소프트 로고
+int main(){
+    cout << "       _.-;;-._\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-;;-._|\n";
+    cout << "'-..-'|   ||   |\n";
+    cout << "'-..-'|_.-''-._|\n";
+
+    return 0;
+}
