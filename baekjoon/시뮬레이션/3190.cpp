37cceb20c07becb3c24a0f93d936935cdf9ef24f
h3136514 <3136514@naver.com>
2025-04-21 22:17:12 +0900

    refactor: 뱀

--- baekjoon/시뮬레이션/3190.cpp
+++ baekjoon/시뮬레이션/3190.cpp
@@ -2,6 +2,81 @@
 #include <queue>
 
 using namespace std;
+int N, L, K, D, row, col, X, snackX = 1, snackY = 1, board[101][101], CNT;
+int offer_X[4] = {1, 0, -1, 0};
+int offer_Y[4] = {0, 1, 0, -1};
+char C;
+queue<pair<int, int>> snake;
+queue<pair<int, char>> rotateS;
+
+// 벽 확인
+bool check(int x, int y){
+    return x > 0 && x <= N && y > 0 && y <= N;
+}
+
+// 뱀
+int main(){
+    cin >> N;
+    cin >> K;
+    for(int i = 0; i < K; i++){
+        cin >> col >> row;
+        board[col][row] = 1;    // 사과 : 1
+    }
+    cin >> L;
+    for(int i = 0; i < L; i++){
+        cin >> X >> C;
+        rotateS.push({X, C});
+    }
+
+    // 처음 뱀의 위치 대입
+    snake.push({snackX, snackY});
+    board[snackY][snackX] = 2;  // 뱀 : 2
+    D = 0;  // 오른쪽
+
+    while(true){
+        CNT++;
+        int x = snackX + offer_X[D];
+        int y = snackY + offer_Y[D];
+        // 가야할 곳이 벽 or 자기 몸일 때
+        if(!check(x, y)|| board[y][x] == 2)
+            break;
+
+        // 뱀 이동
+        if(board[y][x] != 1){   // 사과가 없을 경우(뱀의 꼬리 부분을 줄임(길이변화X))
+            int ix = snake.front().first;
+            int iy = snake.front().second;
+            board[iy][ix] = 0;
+            snake.pop();
+        }
+        snake.push({x, y});
+        snackX = x, snackY = y;
+        board[snackY][snackX] = 2;
+
+        // 뱀 방향확인
+        if(!rotateS.empty()){
+            if(CNT == rotateS.front().first){
+                char id = rotateS.front().second;
+                if(id == 'D')
+                    D = (D+1 >= 4) ? 0 : D + 1; // 시계방향 90도
+                else
+                    D = (D-1 < 0) ? 3 : D - 1;  // 반시계방향 90도
+
+                rotateS.pop();
+            }
+        }
+
+    }
+    
+    cout << CNT << "\n";
+
+    return 0;
+}
+
+/*
+#include <iostream>
+#include <queue>
+
+using namespace std;
 int N, K, L, X, x, y, R, CNT, num[102][102];
 int offer_X[4] ={1, 0, -1, 0};
 int offer_Y[4] ={0, 1, 0, -1};
@@ -98,3 +173,4 @@ int main() {
 
     return 0;
 }
+    */
