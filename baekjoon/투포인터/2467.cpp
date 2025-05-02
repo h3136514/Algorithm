101a94a33fb48efa6fb42ce36cd7028db9b3b54d
h3136514 <3136514@naver.com>
2025-05-02 21:49:00 +0900

    feat: 용액

---
+++ baekjoon/투포인터/2467.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define INF 2000000001
+using namespace std;
+int N, startX, endX, sum, minDiffer = INF, result[2];
+vector<int> num;
+
+// 용액
+int main() {
+	cin >> N;
+	num.resize(N);
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
+
+	sort(num.begin(), num.end());
+
+	endX = N - 1;
+	while (startX < endX) {
+		sum = num[startX] + num[endX];
+
+		if (abs(sum) < minDiffer){	// 현재까지 0 에 가장 가까웠던 합
+			minDiffer = abs(sum);
+			result[0] = num[startX];
+			result[1] = num[endX];
+
+			if (sum == 0)
+				break;
+		}
+
+		if (sum < 0)
+			startX++;
+		else
+			endX--;
+	}
+	
+	cout << result[0] << " " << result[1] << "\n";
+
+	return 0;
+}
