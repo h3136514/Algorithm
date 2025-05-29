bbafe0cae90c3c355162a160dcd6adc8ad183aff
h3136514 <3136514@naver.com>
2025-05-29 18:29:41 +0900

    feat: 와이버스 부릉부릉

---
+++ baekjoon/구현/14645.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int N, K, A, B;
+
+// 와이버스 부릉부릉
+int main(){
+    cin >> N >> K;
+    for(int i = 0; i < N; i++){
+        cin >> A >> B;
+    }
+
+    cout << "비와이\n";
+    return 0;
+}
