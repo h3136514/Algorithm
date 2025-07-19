f583a8168ca9aff198cc66bd05e5f81049050c52
h3136514 <3136514@naver.com>
2025-07-20 02:14:36 +0900

    fix: 맥주 99병

--- baekjoon/구현/17293.cpp
+++ baekjoon/구현/17293.cpp
@@ -10,22 +10,30 @@ int main() {
 
     for(int i = N; i > 0; i--){
 
-        cout << i <<" bottles of beer on the wall, " << i <<" bottles of beer.\n";
-        if(i==1)
-            cout <<"Take one down and pass it around, no bottles of beer on the wall.\n";
-        else if(i == 2) 
+        if(i==1){
+            cout << i <<" bottle of beer on the wall, " << i <<" bottle of beer.\n";
+            cout <<"Take one down and pass it around, no more bottles of beer on the wall.\n";
+        }
+        else if(i == 2){
+            cout << i <<" bottles of beer on the wall, " << i <<" bottles of beer.\n";
             cout <<"Take one down and pass it around, "<< i-1 << " bottle of beer on the wall.\n";
-        else    
+        }
+        else{
+            cout << i <<" bottles of beer on the wall, " << i <<" bottles of beer.\n";
             cout <<"Take one down and pass it around, "<< i-1 << " bottles of beer on the wall.\n";
+        }
+
         cout <<"\n";
 
     }
 
-    cout << "No more bottles of beer on the wall, no more bottles of beer.\n";
-
-    cout << "Go to the store and buy some more, "<< N <<" bottles of beer on the wall.\n";
-
-  
+    if(N == 1){
+        cout << "No more bottles of beer on the wall, no more bottles of beer.\n";
+        cout << "Go to the store and buy some more, "<< N <<" bottle of beer on the wall.\n";
+    }else{
+        cout << "No more bottles of beer on the wall, no more bottles of beer.\n";
+        cout << "Go to the store and buy some more, "<< N <<" bottles of beer on the wall.\n";
+    }
 
     return 0;
 
