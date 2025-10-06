a3bb93de152eea5086d064d150631b67c9fc26ad
h3136514 <3136514@naver.com>
2025-10-06 15:35:21 +0900

    feat: 숫자의 표현

---
+++ programmers/기본연습문제/숫자의 표현.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1; // 자기 자신
+    
+    for(int i = 1; i < n; i++){
+        int sum = i;
+        for(int j = i + 1; j < n; j++){
+            sum += j;
+            if(sum == n){
+                answer++;
+                break;
+            }else if(sum > n)
+                break;
+        }
+    }
+    
+    return answer;
+}
