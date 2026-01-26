8216efe95310039c5d34de66da71c98b91cbb964
h3136514 <3136514@naver.com>
2026-01-26 20:37:55 +0900

    feat: Plane

---
+++ baekjoon/구현/8370.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int n1, k1, n2, k2;
+
+// Plane
+int main(){
+    cin >> n1 >> k1 >> n2 >> k2;
+    
+    cout <<n1*k1 +n2*k2 << "\n";
+
+    return 0;
+}
