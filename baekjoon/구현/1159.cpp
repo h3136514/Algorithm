cf351c19e9e74c48fe9806f01d26263a55d7986f
h3136514 <3136514@naver.com>
2026-01-31 23:16:42 +0900

    feat: 농구 경기

---
+++ baekjoon/구현/1159.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N;
+int alphabet[26];
+string s = "abcdefghijklmnopqrstuvwxyz";
+
+// 농구 경기
+int main(void) {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		string input;
+		cin >> input;
+		alphabet[input[0] - 'a']++;
+	}
+	bool predaja = true;
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] >= 5) {
+			cout << s[i];
+			predaja = false;
+		}
+	}
+	if (predaja)
+        cout << "PREDAJA";
+}
