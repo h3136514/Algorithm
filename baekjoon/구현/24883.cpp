a958424ec23d375b6585b7b5c51c3186d2f87c44
h3136514 <3136514@naver.com>
2026-02-23 22:14:28 +0900

    feat: 자동완성

---
+++ baekjoon/구현/24883.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+char c;
+
+// 자동완성
+int main(){
+    cin >> c;
+
+    if(c == 'N' || c == 'n')
+        cout << "Naver D2\n";
+    else
+        cout << "Naver Whale\n";
+
+    return 0;
+}
