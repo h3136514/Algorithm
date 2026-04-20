8f40a19751b2211c0a075bed793849c14332d201
h3136514 <3136514@naver.com>
2026-04-20 20:29:30 +0900

    feat: 멀리 뛰기

---
+++ programmers/DP/멀리 뛰기.cpp
@@ -0,0 +1,18 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12914
+#include <string>
+#include <vector>
+
+using namespace std;
+long long dp[2001];
+long long answer = 0;
+
+long long solution(int n) {
+    dp[1] = 1, dp[2] = 2;
+    for(int i = 3; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2])%1234567;
+    }
+    
+    answer = dp[n];
+    
+    return answer;
+}
