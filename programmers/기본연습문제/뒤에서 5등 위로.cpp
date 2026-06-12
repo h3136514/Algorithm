3ad90cad156914191324edbb33db301490d9005c
h3136514 <3136514@naver.com>
2026-06-12 17:35:47 +0900

    feat: 뒤에서 5등 위로

---
+++ programmers/기본연습문제/뒤에서 5등 위로.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> answer;
+
+vector<int> solution(vector<int> num_list) {
+    sort(num_list.begin(), num_list.end());
+    
+    for(int i = 5; i < num_list.size(); i++){
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
