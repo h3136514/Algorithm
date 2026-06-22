75a7808d6d7b4a8d8aef9393f5be508f8c2cf77e
h3136514 <3136514@naver.com>
2026-06-22 22:10:42 +0900

    feat: ad 제거하기

---
+++ programmers/기본연습문제/ad 제거하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+vector<string> answer;
+
+vector<string> solution(vector<string> strArr) {
+    for(int i = 0; i < strArr.size(); i++){
+        if(strArr[i].find("ad") == string::npos){
+            answer.push_back(strArr[i]);
+        }    
+    }
+    
+    return answer;
+}
