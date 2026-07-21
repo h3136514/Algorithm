d3ec3f7051f0279903a25a9457934f5aca40db65
h3136514 <3136514@naver.com>
2026-07-21 23:28:02 +0900

    refactor: 노란불 신호등(유클리드호제법 적용)

--- programmers/수학/노란불 신호등.cpp
+++ programmers/수학/노란불 신호등.cpp
@@ -5,11 +5,25 @@
 using namespace std;
 int num[5], cnt[5], maxNum = 1, GYRSize, answer = -1;
 
+// 유클리드호제법 (최대 공약수)
+long long gcd(long long a, long long b){
+    if(!b)
+        return a;
+    return gcd(b, a%b);
+}
+
+// 최소 공배수
+long long llm(long long a, long long b){
+    return a*b / gcd(a, b);
+}
+
 int solution(vector<vector<int>> signals) {
-    GYRSize = signals.size();
+    GYRSize = signals.size(); // 신호등 갯수
+    
     for(int i = 0; i < GYRSize; i++){
-        maxNum *= (signals[i][0] + signals[i][1] + signals[i][2]);
-        cnt[i] = signals[i][0];
+        int sum = (signals[i][0] + signals[i][1] + signals[i][2]);
+        maxNum = llm(maxNum, (long long)sum);   // 모든 신호등 합의 최소 공배수
+        cnt[i] = signals[i][0]; // 신호등별 현재 남은 초
     } 
       
     for(int i = 1; i <= maxNum; i++){
@@ -21,7 +35,7 @@ int solution(vector<vector<int>> signals) {
             }
         }
         
-        if(check){
+        if(check){  // 모든 신호등이 노란불(index == 1)일 때
             answer = i;
             break;
         }
