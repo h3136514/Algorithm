e87fc0d198b7e6d933bb0b24f54c68b67d41980d
h3136514 <3136514@naver.com>
2026-08-11 23:25:05 +0900

    feat: 카운트 다운

---
+++ programmers/기본연습문제/카운트 다운.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+vector<int> answer;
+
+vector<int> solution(int start_num, int end_num) {
+    for(int i = start_num; i >= end_num; i--){
+        answer.push_back(i);
+    }
+    
+    return answer;
+}
